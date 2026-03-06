91c06317b244f621ec30addd37eb4716b073e2ef
Pedro <pedro@semiotic.ai>
2026-03-06 19:29:30 -0300

    reset mason lsp config settings and remove opencode opts

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,23 +1,11 @@
 return {
-  {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {
-      automatic_installation = false,
-      ensure_installed = { "ts_ls", "eslint" },
-    },
-  },
-
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        rust_analyzer = {},
-      },
-
-      setup = {
-        rust_analyzer = function()
-          return true
-        end,
+  "mason-org/mason.nvim",
+  opts = {
+    ui = {
+      icons = {
+        package_installed = "✓",
+        package_pending = "➜",
+        package_uninstalled = "✗",
       },
     },
   },
--- lua/plugins/opencode.lua
+++ lua/plugins/opencode.lua
@@ -2,7 +2,6 @@ return {
   "NickvanDyke/opencode.nvim",
   dependencies = {
     -- Recommended for better prompt input, and required to use `opencode.nvim`'s embedded terminal — otherwise optional
-    { "folke/snacks.nvim", opts = { input = { enabled = true } } },
   },
   config = function()
     vim.g.opencode_opts = {
